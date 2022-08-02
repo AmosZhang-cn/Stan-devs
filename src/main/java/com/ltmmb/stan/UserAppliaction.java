029a6409dd858a696b4f6cf2822caf4c054780bd
Stanzhang <kuaibu2017@163.com>
2022-08-03 00:26:10 +0800

    Stan-dev Swaggger

--- src/main/java/com/ltmmb/stan/UserAppliaction.java
+++ src/main/java/com/ltmmb/stan/UserAppliaction.java
@@ -1,15 +1,10 @@
 package com.ltmmb.stan;
 
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
-
-@MapperScan("com.ltmmb.stan.dao") //指定dao层所在的位置
+//@MapperScan("com.ltmmb.stan.dao") //指定dao层所在的位置
 @SpringBootApplication
 public class UserAppliaction {
     public static void main(String[] args) {
